2f07fbecede2d3bce7d1cc1f7cbaf1eb63156c28
qunlong <jinqunlong@zuzuche.com>
2019-05-15 17:30:57 +0800

    配置后台管理工具页

---
+++ tools_website/tools/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config = 'tools.apps.ToolsConfig'
--- tools_website/tools/admin.py
+++ tools_website/tools/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+import xadmin
+from .models.typemodels import Type
 # Register your models here.
+class TypeAdminModel(object):
+    list_display = ['id', 'name', 'status', 'add_time', 'update_time', 'add_user']
+    fields = ['name', 'status', 'parent']
+    search_fields = ['name', 'parent']
+
+
+xadmin.site.register(Type, TypeAdminModel)
